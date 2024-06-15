68d8dfe59d63048a5f11ca85c89b52aef1496408
Nthumo <emmanuelmutinda235@gmail.com>
2024-06-15 16:17:28 +0300

    getintouch:1

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import home, skills, intouch, about, python
+from .views import home, skills, contact_view, about, python
 
 app_name = 'portfolio'
 urlpatterns = [
     path('', home, name='home'),
     path('skills/', skills, name='skills'),
     path('prolang/python', python, name='python'),
-    path('getintouch/', intouch, name='getintouch'),
+    path('contact/', contact_view, name='contact'),
     path('about/', about, name='about'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
+from .forms import ContactForm
 
-# Create your views here.
+
+#contactform view
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            comment = form.cleaned_data['comment']
+            send_mail(
+                f"New contact form submission from {name}",
+                comment,
+                email,
+                [settings.DEFAULT_FROM_EMAIL],
+                fail_silently=False,
+            )
+            return render(request, 'contact_success.html')
+    else:
+        form = ContactForm()
+
+    return render(request, 'portfolio/contact.html', {'form': form})
+
+# home view.
 def home(request):
     return render(request, 'portfolio/home.html')
 
@@ -17,5 +42,5 @@ def python(request):
     return render(request, 'portfolio/python.html')
 
 
-def intouch(request):
-    return render(request, 'portfolio/getintouch.html')
+#def intouch(request):
+#    return render(request, 'portfolio/contact.html')
